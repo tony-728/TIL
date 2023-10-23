05495ddbe00198ba9a6e4e575e1a7b4fd4fa37d9
tony-728 <ksang0728@gmail.com>
2023-10-24 01:38:02 +0900

    modify

--- ML/linear_regression/LMwithsklearn.py
+++ ML/linear_regression/LMwithsklearn.py
@@ -1,9 +1,6 @@
-# %%
 import pandas as pd
 import numpy as np
-
-# %%
-
+from sklearn.linear_model import LinearRegression
 
 column = [
     "CRIM",
@@ -23,34 +20,19 @@ column = [
 ]
 data = pd.read_csv("housing.csv", names=column)
 
-# %%
 df = data[["RM", "LSTAT", "MEDV"]]
 
-# %%
 for c in ["RM", "LSTAT", "MEDV"]:
     m = df[c].mean()
     s = df[c].std()
 
     df[c] = (df[c] - m) / s
 
-
-# %%
 rm = np.array(df["RM"])
 lstat = np.array(df["LSTAT"])
 
 X = np.stack([rm, lstat], axis=-1)
 y = np.array(df["MEDV"])
 
-# %%
-# sklearn 사용
-import numpy as np
-from sklearn.linear_model import LinearRegression
-
-# %%
 reg = LinearRegression().fit(X, y)
-
-# %%
-reg.coef_
-
-# %%
-reg.intercept_
+print(reg.coef_, reg.intercept_)
